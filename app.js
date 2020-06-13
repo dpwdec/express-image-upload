eee924a4fc661624b93587c83c2db21f4fbf78a6
dpwdec <declankolakowski@gmail.com>
2020-06-13 14:11:26 +0100

    images now save to the upload_image database when uploaded

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const mongoose = require('mongoose');
+// connect to mongoose database
+mongoose.connect('mongodb://localhost/upload_image', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, err => { 
+  if(err) { console.log(err); return; }
+  console.log('connected')  
+});
+
 const multer = require('multer');
 var upload = multer({dest: './uploads'});
 
@@ -13,8 +23,22 @@ app.set('view engine', '.hbs');
 
 app.get('/', (req, res) => res.render('index.hbs'));
 
+const fs = require('fs');
+const Image = require('./models/image.js');
+
 app.post('/', upload.single('image'), (req, res) => {
-  console.log(req.file);
+
+  var uploadedImage = new Image({
+    img: {
+      data: fs.readFileSync('./uploads/' + req.file.filename),
+      imgType: req.file.mimetype
+    }
+  });
+
+  uploadedImage.save(err => {
+    if(err) { console.log(err); return; }
+    console.log('image saved');
+  })
 });
 
 app.listen(port);
