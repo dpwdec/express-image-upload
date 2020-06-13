504011a71ccc9795c3f70a6e3b796fca71609137
dpwdec <declankolakowski@gmail.com>
2020-06-13 13:08:50 +0100

    simplified multer routes.

--- app.js
+++ app.js
@@ -3,18 +3,7 @@ const app = express();
 const port = 3000;
 
 const multer = require('multer');
-
-var storage = multer.diskStorage({ 
-  destination: (req, file, cb) => { 
-      cb(null, 'uploads') 
-  }, 
-  filename: (req, file, cb) => { 
-      cb(null, file.fieldname + '-' + Date.now()) 
-  } 
-}); 
-
-var upload = multer({ storage: storage }); 
-
+var upload = multer({dest: './uploads'});
 
 const exphbs = require('express-handlebars');
 //add the express-handlebars view engine to express
@@ -25,7 +14,7 @@ app.set('view engine', '.hbs');
 app.get('/', (req, res) => res.render('index.hbs'));
 
 app.post('/', upload.single('image'), (req, res) => {
-  
+  console.log(req.file);
 });
 
 app.listen(port);
