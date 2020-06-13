b384b48013585533b73355d51c43c2c8c74e1b87
dpwdec <declankolakowski@gmail.com>
2020-06-13 11:27:01 +0100

    set up handlebars views.

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('This is an image upload app!'));
+
+const exphbs = require('express-handlebars');
+//var hbs = exphbs.create();
+//add the express-handlebars view engine to express
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+// set the view engine for your app
+app.set('view engine', '.hbs');
+
+app.get('/', (req, res) => res.render('index.hbs'));
 
 app.listen(port);
